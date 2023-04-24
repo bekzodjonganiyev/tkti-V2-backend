f19301be2d252a5b1f8d992c2da989f5012fc7cd
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-04-24 09:36:49 +0500

    aa

--- controller/photos/validate.js
+++ controller/photos/validate.js
@@ -5,7 +5,7 @@ class Validate{
     postPhoto = joi.object().keys({
         width: joi.string().min(3).required(),
         height: joi.string().min(3).required(),
-        date: joi.string().required()
+        date: joi.string()
     })
 
 }
