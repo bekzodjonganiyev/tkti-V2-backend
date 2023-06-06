4f77957e10cb5abc373dd9b4556f556a39a461f8
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-06-06 05:59:53 +0500

    mongodb uri clonsole log

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV !== "production") {
 const app = express()
 const mongoose = require("mongoose")
 // mongoose.connect("mongodb+srv://masanov:masanov3167@cluster0.ss2bslz.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true}).then(()=>{console.log("success ")}).catch((err)=>{console.log(err)})
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true}).then(()=>{console.log("success ")}).catch((err)=>{console.log("error")})
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true}).then(()=>{console.log("success", process.env.MONGODB_URI)}).catch((err)=>{console.log("error")})
 
 app.use(cors())
 app.use(express.json())
