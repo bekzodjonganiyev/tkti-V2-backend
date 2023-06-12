20ddfc6cdcfae0467a3814c208a96d3434c77e0b
Masanov Jumabek <masanov3167@gmail.com>
2023-06-12 11:55:46 +0500

    search by title

--- controller/news/index.js
+++ controller/news/index.js
@@ -21,14 +21,12 @@ class News {
       const News = new News_data(value);
       await News.save();
 
-      res
-        .status(200)
-        .json({
-          status: 200,
-          success: true,
-          message: `Yangilik qo'shildi`,
-          data: News,
-        });
+      res.status(200).json({
+        status: 200,
+        success: true,
+        message: `Yangilik qo'shildi`,
+        data: News,
+      });
     } catch (e) {
       if (req.file) {
         removeMedia(req.file.filename);
@@ -72,14 +70,12 @@ class News {
         { new: true }
       );
 
-      res
-        .status(200)
-        .json({
-          status: 200,
-          success: true,
-          message: `yangilik yangilandi`,
-          data: updated,
-        });
+      res.status(200).json({
+        status: 200,
+        success: true,
+        message: `yangilik yangilandi`,
+        data: updated,
+      });
     } catch (e) {
       if (req.file) {
         removeMedia(req.file.filename);
@@ -95,24 +91,22 @@ class News {
     try {
       // const News = await News_data.find().sort({_id:-1});
       const { page, category } = req.query;
-      const totalCount = await News_data.find({category}).sort({ date: -1 });
-      const total = totalCount?.length
+      const totalCount = await News_data.find({ category }).sort({ date: -1 });
+      const total = totalCount?.length;
       const News = totalCount.splice(
         page && page > 1 ? Number(page) * 10 - 10 : 0,
         10
       );
 
-      res
-        .status(200)
-        .json({
-          status: 200,
-          success: true,
-          message: `Yaxshi uka`,
-          total: total,
-          perPage: 10,
-          currentPage: News?.length,
-          data: News,
-        });
+      res.status(200).json({
+        status: 200,
+        success: true,
+        message: `Yaxshi uka`,
+        total: total,
+        perPage: 10,
+        currentPage: News?.length,
+        data: News,
+      });
     } catch (e) {
       console.log(e);
       res
@@ -123,22 +117,33 @@ class News {
 
   async GetById(req, res) {
     try {
-      const News = await News_data.findOne({ _id: req.params.id });
+      // const News = await News_data.findOne({ _id: req.params.id });
+      const filtered = (title) => {
+        const replaced = title.replace(/['~"#“‘`]+/g, "");
+        const slug = replaced
+          .toLowerCase()
+          .replace(/[^a-z0-9]+/g, "-")
+          .replace(/^-+|-+$/g, "");
+        return slug;
+      };
 
-      if (!News) {
+      const News = await News_data.find();
+      const finded = News.find(
+        (item) => filtered(item.title_uz) == req.params.id
+      );
+      // console.log(finded);
+      if (!finded) {
         res
           .status(404)
           .json({ status: 404, message: "yangilik topilmadi", success: false });
         return;
       }
-      res
-        .status(200)
-        .json({
-          status: 200,
-          success: true,
-          message: `Yaxshi uka`,
-          data: News,
-        });
+      res.status(200).json({
+        status: 200,
+        success: true,
+        message: `Yaxshi uka`,
+        data: finded,
+      });
     } catch (e) {
       console.log(e);
       res
@@ -154,14 +159,12 @@ class News {
         res.status(404).json({ status: 404, message: "yangilik topilmadi :(" });
         return;
       }
-      res
-        .status(200)
-        .json({
-          status: 200,
-          success: true,
-          message: `Yaxshi  delet qilindi`,
-          data: News,
-        });
+      res.status(200).json({
+        status: 200,
+        success: true,
+        message: `Yaxshi  delet qilindi`,
+        data: News,
+      });
     } catch (e) {
       console.log(e);
       res
