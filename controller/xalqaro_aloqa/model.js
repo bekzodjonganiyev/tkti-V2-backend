a5d224eba3fdc4c8681e24612e657886a36f1007
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-07-03 12:06:12 +0500

    Revert "fixed tkti"
    This reverts commit 294b77e611a84d3a5e829c895797a9e76fa9eb58.

--- controller/xalqaro_aloqa/model.js
+++ controller/xalqaro_aloqa/model.js
@@ -5,9 +5,11 @@ const modelSchema = new Schema({
   title_uz: String,
   title_ru: String,
   title_en: String,
+  title_ar: String,
   body_uz: String,
   body_ru: String,
   body_en: String,
+  body_ar: String,
   category: {
     type: String,
     enum: ["a", "b"],
